f66af638151ac354cf084de0f1257b93c923dcb7
Kenneth Swan <ken@Kenneths-MacBook-Air.local>
2019-10-01 15:31:52 -0500

    Added class for tomagotchi as well as properties

---
+++ js/app.js
@@ -0,0 +1,19 @@
+class Tomagotchi {
+	constructor(age){
+		this.boredom = 10;		
+		this.sleepiness = 10;
+		this.hunger = 10;
+		this.name = "The big lagotchi"; 					//1 interval, no more than one. 
+} 
+}
+const game = {
+//time
+//print values. When one changes, show them all. 
+// don't try to over think it. One step at a time. 
+} 
+
+
+
+//Listeners {
+
+//}
