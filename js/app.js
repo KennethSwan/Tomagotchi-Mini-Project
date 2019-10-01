366c1f8a398eed813865ed97eb067c9945fb2d96
Kenneth Swan <ken@Kenneths-MacBook-Air.local>
2019-10-01 15:46:03 -0500

    Added greeting

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 class Tomagotchi {
 	constructor(age){
-		this.boredom = 10;		
-		this.sleepiness = 10;
-		this.hunger = 10;
+		this.boredom = [1,2,3,4,5,6,7,8,9,10];		
+		this.sleepiness = [1,2,3,4,5,6,7,8,9,10];
+		this.hunger = [1,2,3,4,5,6,7,8,9,10];
 		this.name = "The big lagotchi"; 					//1 interval, no more than one. 
-} 
+	} 
+	greet(){
+		console.log('I`m the dude, man!');
+	}
 }
 const game = {
 //time
