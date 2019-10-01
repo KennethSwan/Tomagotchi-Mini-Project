3ce91f7e60a38e1cc8f482fa16e102ae017743a9
Kenneth Swan <ken@Kenneths-MacBook-Air.local>
2019-10-01 15:54:45 -0500

    add statements for hunger, sleepiness, boredom, and death

--- js/app.js
+++ js/app.js
@@ -1,13 +1,26 @@
 class Tomagotchi {
-	constructor(age){
+	constructor(){
 		this.boredom = [1,2,3,4,5,6,7,8,9,10];		
 		this.sleepiness = [1,2,3,4,5,6,7,8,9,10];
 		this.hunger = [1,2,3,4,5,6,7,8,9,10];
 		this.name = "The big lagotchi"; 					//1 interval, no more than one. 
 	} 
-	greet(){
-		console.log('I`m the dude, man!');
+	name(){
+		console.log("Hey!" + ' I`m the dude, man!');
 	}
+	hunger(){
+		console.log("I`m starving, man!");
+	}
+	sleepines(){
+		console.log("I need to get some sleep, man!");
+	}
+	boredom(){
+		console.log("I'm so bored, man! Let's bowl or something!");
+	}
+	die(){
+		console.log("This isn`t `nam Walter! There are rules!");		
+	}
+
 }
 const game = {
 //time
