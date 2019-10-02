e3c6f34c5d69336b72f2f5d498460d90e7d9c388
Kenneth Swan <ken@Kenneths-MacBook-Air.local>
2019-10-02 14:56:51 -0500

    Added timer to HTML

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 class Tomagotchi {
 	constructor(name){
-		this.boredom = [1,2,3,4,5,6,7,8,9,10];	//
-		this.sleepiness = [1,2,3,4,5,6,7,8,9,10]; // These are starting parameters, will need to 
-		this.hunger = [1,2,3,4,5,6,7,8,9,10]; // increase as time goes on. 
-		this.age = "28 weeks";
+		this.boredom = [];	//
+		this.sleepiness = []; // These are starting parameters, will need to 
+		this.hunger = []; // increase as time goes on. 
+		this.age = 0;
 		this.name = name; 		//1 interval, no more than one. Use setInterval()
 	} 
 	nameGreet(){
@@ -20,26 +20,40 @@ class Tomagotchi {
 	}
 	dieGreet(){
 		console.log("This isn`t `nam Walter! There are rules!");		
-	};
+	}
 }
 // const pet = new Tomagotchi();
 
 
 const game = {
 	pet: null,
+
 //time
 //print values. When one changes, show them all. 
 // don't try to over think it. One step at a time.
-// need to set up a timer for each property to get to zero 
+// need to set up a timer for each property to get to ten 
 //Prompt user to do something if the level of one or 
 //more properties gets to zero. 
-// When player clicks a button, increase that property by 1
+// When player clicks a button, decrease that property by 1
 	start(name){
 		const dude = new Tomagotchi(name);
-		console.log(dude);
+		// console.log(dude);
 		this.pet = dude;
-	}
+	},
+ 
+ 	setTimer() {
+	const $timer = $('#timer');
+	const interval = setInterval(() =>{
+		if(this.time === 0){
+			clearInterval(interval);
+			this.age++
+		}	else{
+			this.time--
+		}
+		$timer.text(`timer: ${this.time}s`)
+	}, 10000)
 } 
+}
 //Listeners {
 // Add listener for feeding, going to sleep, and playing
 // Make character move around screen randomly as time pass.
@@ -49,10 +63,10 @@ const game = {
 // 	$('.Wake Up Dude')
 // }
  $('form').on('submit', (event) => {
-    console.log('starting game');  
-    console.log( $('#input-box').val() );
-    let petName = $('#input-box').val();
     event.preventDefault();
+    console.log('starting game');  
+    console.log( $('#input-box').val() + ' is my name!' );
+    let petName = $('#input-box').val();
 	game.start(petName)  
   });
 
