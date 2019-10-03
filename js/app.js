09fc34b40f80e0390f911cfe1e29d6b2f099dde6
Kenneth Swan <ken@Kenneths-MacBook-Air.local>
2019-10-03 07:54:06 -0500

    added image

--- js/app.js
+++ js/app.js
@@ -1,98 +1,103 @@
 class Tomagotchi {
-	constructor(name){
-		this.boredom = 0;	//
-		this.sleepiness = 0; // These are starting parameters, will need to 
-		this.hunger = 0; // increase as time goes on. 
-		this.age = 0;
-		this.name = name; 		//1 interval, no more than one. Use setInterval()
-	} 
-	nameGreet(){
-		console.log("Hey! I`m " + " ,man!");
-	}
-	hungerGreet(){
-		console.log("I`m starving, man!");
-	}
-	sleepinesGreet(){
-		console.log("I need to get some sleep, man!");
-	}
-	boredomGreet(){
-		console.log("I'm so bored, man! Let's bowl or something!");
-	}
-	dieGreet(){
-		console.log("This isn`t `nam Walter! There are rules!");		
-	}
+    constructor(name) {
+        this.boredom = 0; //
+        this.sleepiness = 0; // These are starting parameters, will need to 
+        this.hunger = 0; // increase as time goes on. 
+        this.age = 0;
+        this.name = name; //1 interval, no more than one. Use setInterval()
+    }
+    nameGreet() {
+        console.log("Hey! I`m " + " ,man!");
+    }
+    hungerGreet() {
+        console.log("I`m starving, man!");
+    }
+    sleepinesGreet() {
+        console.log("I need to get some sleep, man!");
+    }
+    boredomGreet() {
+        console.log("I'm so bored, man! Let's bowl or something!");
+    }
+    dieGreet() {
+        console.log("This isn`t `nam Walter! There are rules!");
+    }
 }
 // const pet = new Tomagotchi();
 
 const game = {
-	pet: null,
-	time: 0,
+    pet: null,
+    time: 0,
 
-	//time
-	//print values. When one changes, show them all. 
-	// don't try to over think it. One step at a time.
-	// need to set up a timer for each property to get to ten 
-	//Prompt user to do something if the level of one or 
-	//more properties gets to zero. 
-	// When player clicks a button, decrease that property by 1
-	start(name){
+    //time
+    //print values. When one changes, show them all. 
+    // don't try to over think it. One step at a time.
+    // need to set up a timer for each property to get to ten 
+    //Prompt user to do something if the level of one or 
+    //more properties gets to zero. 
+    // When player clicks a button, decrease that property by 1
+    start(name) {
 
-		// instantiate the Tomagotchi class
-		// dude is now an object made from the Toma class
-		const dude = new Tomagotchi(name);
+        // instantiate the Tomagotchi class
+        // dude is now an object made from the Toma class
+        const dude = new Tomagotchi(name);
 
-		// dude is being set as a property of (variable) in the game object.
-		this.pet = dude;
+        // dude is being set as a property of (variable) in the game object.
+        this.pet = dude;
 
-		this.setTimer()
-	},
+        this.setTimer()
+    },
 
-	// call this any time any value changes
-	printProps() {
-		const $age = $('#age')
-		$age.text(`age: ${this.pet.age}`)
-		const $hunger = $('#hunger');
-		$hunger.text(`hunger: ${this.pet.hunger}`);
-		const $timer = $('#timer');
-		$timer.text(`timer: ${this.time}s`)
-		const $boredom = $('#boredom');
-		$boredom.text(`boredom: ${this.pet.boredom}`)
-		const $sleepiness = $('#sleepiness');
-		$sleepiness.text(`sleepiness: ${this.pet.sleepiness}`)
+    // call this any time any value changes
+    printProps() {
+        const $age = $('#age')
+        $age.text(`age: ${this.pet.age}`)
+        const $hunger = $('#hunger');
+        $hunger.text(`hunger: ${this.pet.hunger}`);
+        const $timer = $('#timer');
+        $timer.text(`timer: ${this.time}s`)
+        const $boredom = $('#boredom');
+        $boredom.text(`boredom: ${this.pet.boredom}`)
+        const $sleepiness = $('#sleepiness');
+        $sleepiness.text(`sleepiness: ${this.pet.sleepiness}`)
+    },
 
+    stayingAlive() {
+    	$('#feed').on('click', (event) => {
+    		// this.pet.hunger--
+    	}); 
+        console.log('It`s alive!');   
+    },
 
-	},
- 
- 	setTimer(){
-		const interval = setInterval(() => {	
-			this.time++
-			
-			if (this.time % 10 === 0) {
-				this.pet.age++;
-			}
-			if (this.time % 5 === 0) {
-				this.pet.hunger++;	
-			}
-			if (this.pet.hunger == 10){
-				console.log("Pet has died of hunger!");
-			}
-			if (this.time %8 === 0) {
-				this.pet.boredom++;
-			}
-			if (this.pet.boredom == 10){
-				console.log("Pet has died of boredom!");
-			}
-			if (this.time %3 === 0) {
-				this.pet.sleepiness++;
-			}
-			if (this.pet.sleepiness == 10) {
-				console.log("Pet has died of lack of sleep!");
-			}
+	setTimer() {
+    const interval = setInterval(() => {
+        this.time++
 
-			this.printProps() // call this any time any value changes
+        if (this.time % 10 === 0) {
+            this.pet.age++;
+        }
+        if (this.time % 5 === 0) {
+            this.pet.hunger++;
+        }
+        if (this.pet.hunger == 10) {
+            console.log("Pet has died of hunger!");
+        }
+        if (this.time % 8 === 0) {
+            this.pet.boredom++;
+        }
+        if (this.pet.boredom == 10) {
+            console.log("Pet has died of boredom!");
+        }
+        if (this.time % 3 === 0) {
+            this.pet.sleepiness++;
+        }
+        if (this.pet.sleepiness == 10) {
+            console.log("Pet has died of lack of sleep!");
+        }
 
-		}, 1000)
-	} 
+        this.printProps() // call this any time any value changes
+
+    }, 1000)
+}
 }
 //Listeners {
 // Add listener for feeding, going to sleep, and playing
@@ -104,14 +109,13 @@ const game = {
 // }
 $('form').on('submit', (event) => {
     event.preventDefault();
-    console.log('starting game');  
-    console.log( $('#input-box').val() + ' is my name!' );
+    console.log('starting game');
+    console.log($('#input-box').val() + ' is my name!');
     let petName = $('#input-box').val();
-	game.start(petName)  
+    game.start(petName)
 });
 
 
 // 1. print values method that does it jQuery
 // 2. get hungrier as time passes -- use your print values function
 // 3. pressing button should change hunger
-
